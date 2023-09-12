eda52a85c8cf09ea793af39b9121e6125338928f
FantasmasDe <fantasmas.dev@gmail.com>
2023-09-12 23:29:02 +0300

    1. добавлены действия для кнопки "поделиться приложением" 2. добавлены действия для кнопки "поддержки" 3. добавлены действия для кнопки "пользовательское соглашение"

--- app/src/main/java/com/example/playlistmaker/SettingsAcivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsAcivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -18,6 +19,27 @@ class SettingsAcivity : AppCompatActivity() {
 
         binding.settingHomeButton.setOnClickListener {
             finish()
+        }
+
+        binding.sharingAppButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type="text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT,"https://practicum.yandex.ru/android-developer/?from=catalog")
+            startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+        }
+
+        binding.supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("fragofikus@gmail.com"))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            supportIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            startActivity(supportIntent)
+        }
+
+        binding.termsOfUseButton.setOnClickListener {
+            val termsIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(termsIntent)
         }
     }
 }
