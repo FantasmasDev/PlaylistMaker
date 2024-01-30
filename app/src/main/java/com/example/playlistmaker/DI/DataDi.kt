5e1eaa94c4d08556e3775673065c04f6d39c9384
FantasmasDe <fantasmas.dev@gmail.com>
2024-01-30 11:28:29 +0300

    1. koin refactoring 2. include sharedPrefs 3. include Retrofit

--- app/src/main/java/com/example/playlistmaker/DI/DataDi.kt
+++ app/src/main/java/com/example/playlistmaker/DI/DataDi.kt
@@ -1,7 +1,9 @@
 package com.example.playlistmaker.DI
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.example.playlistmaker.data.NetworkClient
+import com.example.playlistmaker.data.network.ItunesApiService
 import com.example.playlistmaker.data.network.RetrofitNetworkClient
 import com.example.playlistmaker.data.repository.PlayerRepositoryImpl
 import com.example.playlistmaker.data.repository.SharedRepositoryImpl
@@ -11,27 +13,55 @@ import com.example.playlistmaker.data.storage.UserStorage
 import com.example.playlistmaker.domain.repository.PlayerRepository
 import com.example.playlistmaker.domain.repository.SharedRepository
 import com.example.playlistmaker.domain.repository.TrackRepository
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val SHARED_PREFS_NAME = "shared_prefs_name"
 
 val dataModule = module {
-    factory<PlayerRepository>{
+    factory<PlayerRepository> {
         PlayerRepositoryImpl()
     }
 
     single<NetworkClient> {
-        RetrofitNetworkClient(context = get())
+        RetrofitNetworkClient(retrofit = get(), context = get())
     }
 
-    single<TrackRepository>{
+    single<ItunesApiService>{
+        Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(
+                        HttpLoggingInterceptor()
+                            .setLevel(HttpLoggingInterceptor.Level.BODY)
+                    )
+                    .build()
+            )
+            .build()
+            .create(ItunesApiService::class.java)
+    }
+
+
+
+    single<TrackRepository> {
         TrackRepositoryImpl(networkClient = get())
     }
 
-    single<UserStorage>{
-        SharedPrefUserStorage(context = get())
+    single<UserStorage> {
+        SharedPrefUserStorage(sharedPreferences = get(), context = get())
     }
 
-    single<SharedRepository>{
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
+    single<SharedRepository> {
         SharedRepositoryImpl(userStorage = get())
     }
-
 }
--- app/src/main/java/com/example/playlistmaker/data/storage/SharedPrefUserStorage.kt
+++ app/src/main/java/com/example/playlistmaker/data/storage/SharedPrefUserStorage.kt
@@ -1,19 +1,16 @@
 package com.example.playlistmaker.data.storage
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.example.playlistmaker.data.dto.SavedHistoryOfTracks
 import com.example.playlistmaker.data.dto.TrackDto
 import com.example.playlistmaker.data.models.NightMode
 import com.google.gson.Gson
 
-private const val SHARED_PREFS_NAME = "shared_prefs_name"
 private const val THEME_NIGHT_MODE_STATE = "switch_night_mode_state"
 private const val TRACKS_HISTORY = "tracks_history"
 
-class SharedPrefUserStorage(context: Context) : UserStorage {
-
-    private val sharedPreferences =
-        context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+class SharedPrefUserStorage(private val sharedPreferences: SharedPreferences, context: Context) : UserStorage {
 
     override fun saveTheme(isNightMode: NightMode) {
         sharedPreferences.edit().putBoolean(THEME_NIGHT_MODE_STATE, isNightMode.isEnable).apply()
